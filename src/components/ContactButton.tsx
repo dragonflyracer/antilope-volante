24014be67ca57611d389c8701d990c412622b2c2
dragonflyracer <44474969+dragonflyracer@users.noreply.github.com>
2026-07-22 20:19:49 -0400

    link facebook

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -26,7 +26,7 @@ export function ContactButton({ isContactVisible }: ContactButtonProps) {
   const handleClick = () => {
     if (isContactVisible) {
       window.location.href =
-        "https://www.facebook.com/antilopevolante";
+        "https://m.facebook.com/antilopevolante";
       return;
     }
 
@@ -81,7 +81,7 @@ export function ContactButton({ isContactVisible }: ContactButtonProps) {
 
   return isContactVisible ? (
     <a
-      href="https://www.facebook.com/antilopevolante"
+      href="https://m.facebook.com/antilopevolante"
       target="_blank"
       rel="noopener noreferrer"
       className="contact-semi-btn contact-facebook"
